6a60f16c27d2328d16bbe7a6ce8a5dc69ba41b89
Arafat Abdussalam <abdussalamarafat@gmail.com>
2021-12-21 08:03:37 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.00, 3.50 ]
+plt.rcParams["figure.autolayout"] = True
+
+w1_slope = -0.5
+w2_intercept = 4
+distance_from_y_intercept = 5
+alpha = 0.1
+
+xdata = np.arange(-10, 10, 0.5)
+ydata = (w1_slope * xdata) + w2_intercept
+
+w1 = w1_slope + (distance_from_y_intercept * alpha)
+w2 = w2_intercept - alpha
+
+print(f"w1 : {round(w1, 1)} w2: {w2} ")
+ydata2 = (w1 * xdata ) + (w2)
+
+plt.xlim(-10, 10)
+plt.ylim(-10, 10)
+plt.grid()
+plt.plot([-5], [3], marker = "o", markersize =5,markeredgecolor = "red", markerfacecolor = "green")
+
+plt.plot(xdata, ydata)
+plt.plot(xdata, ydata2)
+plt.show()
